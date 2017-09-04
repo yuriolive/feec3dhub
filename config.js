b0b16fbcd0c11db34825ce6ecce0b8f9c09fa607
Yuri Olive <yuriso1994@gmail.com>
2017-09-04 15:46:55 -0300

    fix config

--- config.js
+++ config.js
@@ -7,9 +7,34 @@ var configFile = JSON.parse(fs.readFileSync(configName, 'utf8'));
 process.env.NODE_ENV = 'production';
 
 configFile.server.port = process.env.PORT;
-configFile.database.connection = process.env.CLEARDB_DATABASE_URL;
+
+// remove mysql://
+connectionString = process.env.CLEARDB_DATABASE_URL;
+connectionString = connectionString.substring(8, connectionString.length)
+
+// Get user and password
+var userAndPassw = connectionString.split('@')[0];
+var user = userAndPassw.split(':')[0];
+var password = userAndPassw.split(':')[1];
+
+// Host and DB
+var hostAndDb = connectionString.split('@')[1];
+hostAndDb = hostAndDb.split('?')[0];
+
+var host = hostAndDb.split('/')[0];
+var db = hostAndDb.split('/')[1];
+
+configFile.database.connection = {
+    "host": host,
+    "user": user,
+    "password": password,
+    "database": db
+}
+
 configFile.mail.options.auth.user = process.env.MAILGUN_SMTP_LOGIN;
 configFile.mail.options.auth.pass = process.env.MAILGUN_SMTP_PASSWORD;
+
+console.log(configFile);
 
 // Write modified config JSON file
 fs.writeFileSync(configName, JSON.stringify(configFile));
